7460ae52455ab996df764c0808dac377846038ff
Blair Kirkpatrick <blair.kirkpatrick@enodatech.com>
2025-12-02 13:23:29 +0000

    Day2 - first star!

--- cmd/main.go
+++ cmd/main.go
@@ -1,100 +1,144 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	s "strings"
 )
 
 func main() {
-	path := "../testdata/day1.txt"
-	var previousPosition int = 50
-	var currentPosition int = 50 // initial condition
-	var zeroCounter int = 0
-	var fullTurnCounter int = 0
-	var flipCounter int = 0
-	var nudgeCounter int = 0
+	path := "../testdata/day2.txt"
+	var idList []string
+	var invalid bool
+	var nInvalidID int
+	var invalidSum int64 = 0
+	//var invalidIdList []string
 
 	// Read in our data line by line
-	lines, err := readLines(path)
+	dat, err := os.ReadFile(path)
 	if err != nil {
 		log.Printf("Problem reading input file")
 	}
+	idRanges := string(dat)
 
-	fmt.Printf("The dial starts by pointing at %v\n", currentPosition)
+	// Split that single string up, splitting at ","
+	idList = s.Split(idRanges, ",")
 
-	// Go through each line and extract direction and distance of rotation
-	for _, line := range lines {
-		dir, dist, err := parseMessage(line)
-		if err != nil {
-			log.Printf("Error parsing message: %v\n", err)
-		}
-		// I don't just care about phase offset anymore = ^.^ = ffs
-		// fullTurns will record quotient of division
-		fullTurns := dist / 100
-		fullTurnCounter += fullTurns
-		partialTurn := dist % 100 // this is how much my position is going to change.
+	for _, idRange := range idList {
+		fmt.Printf("ID RANGE: %v\n", idRange)
+		firstID, lastID := getFirstLastID(idRange)
+		startSearching, _ := strconv.Atoi(firstID)
+		stopSearching, _ := strconv.Atoi(lastID)
 
-		if dir == "R" {
-			currentPosition = previousPosition + partialTurn
-		} else if dir == "L" {
-			currentPosition = previousPosition - partialTurn
-		}
-
-		if currentPosition == 0 {
-			zeroCounter += 1
-		} else if currentPosition > 99 {
-			if previousPosition != 0 {
-				nudgeCounter += 1
+		for id := startSearching; id <= stopSearching; id++ {
+			//fmt.Printf("checking %v\n", id)
+			idStr := strconv.Itoa(id)
+			evenLength := hasEvenLength(idStr)
+			if evenLength {
+				invalid = isInvalid(idStr)
+				if invalid {
+					nInvalidID++
+					invalidSum += int64(id)
+					// invalidIdList = append(invalidIdList, idStr)
+					fmt.Printf("INVALID ID: %v\n", idStr)
+				}
 			}
-			currentPosition = currentPosition - 100
-		} else if currentPosition < 0 {
-			if previousPosition != 0 {
-				nudgeCounter += 1
-			}
-			currentPosition += 100
+
 		}
 
-		fmt.Printf("The dial is rotated %v to point at %v, Full turns: %v, zeroes: %v, flips: %v, nudge: %v\n", line, currentPosition, fullTurns, zeroCounter, flipCounter, nudgeCounter)
-		previousPosition = currentPosition
+		//fmt.Printf("%v: %v --> first:%v last:%v\n", i, idRange, firstID, lastID)
 	}
 
-	fmt.Printf("We have %v entries\n", len(lines))
+	fmt.Printf("Found %v Invalid IDs\n", nInvalidID)
+	fmt.Printf("SUM of Invalid IDs = %v\n", invalidSum)
 
-	fmt.Printf("Arrow landed on ZERO %v times\n", zeroCounter)
-	fmt.Printf("Arrow blew past ZERO %v times\n", fullTurnCounter)
-	fmt.Printf("Arrow did a little flip over ZERO %v times\n", flipCounter)
-	fmt.Printf("Arrow nudge over ZERO %v times\n", nudgeCounter)
+	// fmt.Printf("The dial starts by pointing at %v\n", currentPosition)
+	// // Go through each line and extract direction and distance of rotation
+	// for _, line := range lines {
+	// 	dir, dist, err := parseMessage(line)
+	// 	if err != nil {
+	// 		log.Printf("Error parsing message: %v\n", err)
+	// 	}
+	// 	// I don't just care about phase offset anymore = ^.^ = ffs
+	// 	// fullTurns will record quotient of division
+	// 	fullTurns := dist / 100
+	// 	fullTurnCounter += fullTurns
+	// 	partialTurn := dist % 100 // this is how much my position is going to change.
 
-	answer := zeroCounter + fullTurnCounter + flipCounter + nudgeCounter
+	// 	if dir == "R" {
+	// 		currentPosition = previousPosition + partialTurn
+	// 	} else if dir == "L" {
+	// 		currentPosition = previousPosition - partialTurn
+	// 	}
 
-	fmt.Printf("The code is: %v\n", answer)
+	// 	if currentPosition == 0 {
+	// 		zeroCounter += 1
+	// 	} else if currentPosition > 99 {
+	// 		if previousPosition != 0 {
+	// 			nudgeCounter += 1
+	// 		}
+	// 		currentPosition = currentPosition - 100
+	// 	} else if currentPosition < 0 {
+	// 		if previousPosition != 0 {
+	// 			nudgeCounter += 1
+	// 		}
+	// 		currentPosition += 100
+	// 	}
+
+	// 	fmt.Printf("The dial is rotated %v to point at %v, Full turns: %v, zeroes: %v, flips: %v, nudge: %v\n", line, currentPosition, fullTurns, zeroCounter, flipCounter, nudgeCounter)
+	// 	previousPosition = currentPosition
+	// }
+
+	// fmt.Printf("Arrow landed on ZERO %v times\n", zeroCounter)
+	// fmt.Printf("Arrow blew past ZERO %v times\n", fullTurnCounter)
+	// fmt.Printf("Arrow did a little flip over ZERO %v times\n", flipCounter)
+	// fmt.Printf("Arrow nudge over ZERO %v times\n", nudgeCounter)
+
+	// answer := zeroCounter + fullTurnCounter + flipCounter + nudgeCounter
+
+	// fmt.Printf("The code is: %v\n", answer)
 
 }
 
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
+func getFirstLastID(idRange string) (string, string) {
+	ids := s.Split(idRange, "-")
+	firstID := ids[0]
+	lastID := ids[1]
+	return firstID, lastID
 }
 
-func parseMessage(msg string) (string, int, error) {
-	dir := string(msg[0])
-	distStr := string(msg[1:])
-	dist, err := strconv.Atoi(distStr)
-	if err != nil {
-		log.Printf("Could not convert distance to int, err: %v\n", err)
+func hasEvenLength(id string) bool {
+	var good bool
+	length := len(id)
+	if length == 0 {
+		good = false
+	} else if length%2 == 0 {
+		good = true
 	}
-	return dir, dist, err
+	return good
 }
+
+func isInvalid(id string) bool {
+	var invalid bool
+	length := len(id)
+	l := id[:length/2]
+	r := id[length/2:]
+	if l == r {
+		invalid = true
+	} else {
+		invalid = false
+	}
+	return invalid
+}
+
+// func parseMessage(msg string) (string, int, error) {
+// 	dir := string(msg[0])
+// 	distStr := string(msg[1:])
+// 	dist, err := strconv.Atoi(distStr)
+// 	if err != nil {
+// 		log.Printf("Could not convert distance to int, err: %v\n", err)
+// 	}
+// 	return dir, dist, err
+// }
