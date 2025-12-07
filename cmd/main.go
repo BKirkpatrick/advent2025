936e77993df2884896a240de51ee930bd92f6c3f
Blair Kirkpatrick <6220300+BKirkpatrick@users.noreply.github.com>
2025-12-07 14:40:03 +0000

    day 7, star 2

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 	s "strings"
 )
 
 var splitCounter int
-var timeLineCounter int
 
 func main() {
-	input, _ := os.ReadFile("../testdata/day7_test.txt")
+	input, _ := os.ReadFile("../testdata/day7.txt")
 	splitCounter = 0
-	timeLineCounter = 0
 
 	rows := s.Split(s.TrimRight(string(input), "\n"), "\n")
 	h := len(rows)
@@ -29,38 +25,40 @@ func main() {
 
 	fmt.Println("")
 
-	var finalField []string
-	var thisRow string
-	for i := range h - 1 {
-		if i == 0 {
-			finalField = append(finalField, rows[0])
-			thisRow = initiateManifold(rows[0], w)
-		}
-		nextRow := advanceTime(thisRow, rows[i+1], w)
-		finalField = append(finalField, nextRow)
-		thisRow = nextRow
+	timeLines := countTimelines(rows, w)
+	nTimeLines := 0
+
+	for _, v := range timeLines {
+		nTimeLines += v
 	}
 
+	fmt.Printf("\nTimelines: %v\n", nTimeLines)
+
+}
+
+func countTimelines(finalField []string, w int) []int {
+	timeLineCounter := make([]int, w)
 	for i, row := range finalField {
-		fmt.Printf("%v: %v\n", i, row)
+		b := []byte(row)
+		if i == 0 {
+			//special case
+			for j, char := range b {
+				if char == 'S' {
+					timeLineCounter[j] = 1
+				}
+			}
+		} else {
+			for j, char := range b {
+				if char == '^' {
+					timeLineCounter[j-1] += timeLineCounter[j]
+					timeLineCounter[j+1] += timeLineCounter[j]
+					timeLineCounter[j] = 0
+				}
+			}
+		}
+		fmt.Printf("%v\n", timeLineCounter)
 	}
-
-	fmt.Printf("\nTotal Splits: %v\n", splitCounter)
-	fmt.Printf("\nTimelines: %v\n", timeLineCounter)
-
-	// var nums [][]int
-	// for _, row := range rows[:h-1] {
-	// 	fs := strings.Fields(row)
-	// 	arr := make([]int, len(fs))
-	// 	for i, f := range fs {
-	// 		n, _ := strconv.Atoi(f)
-	// 		arr[i] = n
-	// 	}
-	// 	nums = append(nums, arr)
-	// }
-
-	// fmt.Println("part 1:", c(nums, ops))
-	// fmt.Println("part 2:", c2(rows[:h-1], rows[h-1]))
+	return timeLineCounter
 }
 
 // advanceTime uses row1 to update row2 to row2Updated
@@ -82,7 +80,6 @@ func advanceTime(row1 string, row2 string, w int) string {
 			} else if row2[i] == '^' {
 				// Beam has hit splitter
 				splitCounter++
-				timeLineCounter += 2
 				// Splitter stays
 				b[i] = '^'
 				// and positions adjacent to splitter become beams
@@ -113,88 +110,4 @@ func initiateManifold(startRow string, w int) string {
 	}
 	betterStartRow = string(b)
 	return betterStartRow
-}
-
-func c(nums [][]int, ops []rune) int {
-	sum := 0
-	for p := 0; p < len(nums[0]); p++ {
-		acc := nums[0][p]
-		for r := 1; r < len(nums); r++ {
-			if ops[p] == '+' {
-				acc += nums[r][p]
-			} else {
-				acc *= nums[r][p]
-			}
-		}
-		sum += acc
-	}
-	return sum
-}
-
-func c2(lines []string, opLine string) int64 {
-	var opPos []int
-	for i, c := range opLine {
-		if c == '+' || c == '*' {
-			opPos = append(opPos, i)
-		}
-	}
-	result := int64(0)
-
-	for i := range opPos {
-		start := opPos[i]
-		end := len(opLine)
-		if i+1 < len(opPos) {
-			end = opPos[i+1]
-		}
-
-		var colText []string
-		for _, line := range lines {
-			if start < len(line) {
-				if end > len(line) {
-					end = len(line)
-				}
-				colText = append(colText, line[start:end])
-			} else {
-				colText = append(colText, "")
-			}
-		}
-
-		nums := extractVertical(colText)
-		op := rune(opLine[start])
-
-		result += applyOp(nums, op)
-	}
-
-	return result
-}
-
-func extractVertical(lines []string) []int {
-	maxH := len(lines)
-	var out []int
-
-	for col := 0; col < len(lines[0]); col++ {
-		var b strings.Builder
-		for row := range maxH {
-			if col < len(lines[row]) && lines[row][col] != ' ' {
-				b.WriteByte(lines[row][col])
-			}
-		}
-		if b.Len() > 0 {
-			n, _ := strconv.Atoi(b.String())
-			out = append(out, n)
-		}
-	}
-	return out
-}
-
-func applyOp(nums []int, op rune) int64 {
-	acc := int64(nums[0])
-	for _, n := range nums[1:] {
-		if op == '+' {
-			acc += int64(n)
-		} else {
-			acc *= int64(n)
-		}
-	}
-	return acc
 }
