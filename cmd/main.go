99d17815d358bb58b31a7ca3c0bc67483870cabf
Blair Kirkpatrick <blair.kirkpatrick@enodatech.com>
2025-12-10 09:20:40 +0000

    data loaded

--- cmd/main.go
+++ cmd/main.go
@@ -18,38 +18,31 @@ import (
 // and a list of 'basis' vectors (the button circuits)
 // I should also store the joltage requirements - they are going to be needed for part 2...
 
-type Point struct {
-	X int
-	Y int
-}
-
-type Edge struct {
-	A, B Point
+type Data struct {
+	lights   string
+	buttons  [][]int
+	joltages []int
 }
 
 func main() {
-	filepath := "../testdata/day9.txt" // adjust
+	filepath := "../testdata/day10_test.txt" // adjust
 
-	verts, err := loadPoints(filepath)
+	data, err := loadData(filepath)
 	if err != nil {
 		panic(err)
 	}
 
-	n := len(verts)
-	fmt.Printf("N RED TILES = %d\n", n)
+	n := len(data)
+	fmt.Printf("N data = %v\n", n)
 
-	// We have some polygon formed by red tiles
-	edges := buildEdges(verts)
+	for i, j := range data {
+		fmt.Printf("Data[%v]: %v\n", i, j)
+	}
 
-	part1 := maxRectangleAreaAllPairs(verts)
-	part2 := maxRectangleAreaNoCut(verts, edges)
-
-	fmt.Println("Part 1 (no constraints) :", part1)
-	fmt.Println("Part 2 (only red/green) :", part2)
 }
 
-func loadPoints(path string) ([]Point, error) {
-	var pts []Point
+func loadData(path string) ([]Data, error) {
+	var data []Data
 
 	f, err := os.Open(path)
 	if err != nil {
@@ -63,116 +56,48 @@ func loadPoints(path string) ([]Point, error) {
 		if line == "" {
 			continue
 		}
-		coords := s.Split(line, ",")
-		if len(coords) != 2 {
-			return nil, fmt.Errorf("invalid line: %q", line)
-		}
-		x, err1 := strconv.Atoi(coords[0])
-		y, err2 := strconv.Atoi(coords[1])
-		if err1 != nil || err2 != nil {
-			return nil, fmt.Errorf("invalid integers on line: %q", line)
-		}
-		pts = append(pts, Point{X: x, Y: y})
-	}
-	return pts, scanner.Err()
-}
+		// we have a valid line
+		elems := s.Split(line, " ")
+		n := len(elems)
 
-func buildEdges(verts []Point) []Edge {
-	n := len(verts)
-	edges := make([]Edge, n)
-	for i := 0; i < n; i++ {
-		edges[i] = Edge{
-			A: verts[i],
-			B: verts[(i+1)%n],
+		// sort out lights
+		lights := elems[0]
+
+		// sort out joltages
+		var joltageList []int
+		joltages := elems[n-1]
+
+		joltages = s.Replace(joltages, "{", "", 1)
+		joltages = s.Replace(joltages, "}", "", 1)
+
+		joltagesSplit := s.Split(joltages, ",")
+
+		for _, j := range joltagesSplit {
+			jInt, _ := strconv.Atoi(j)
+			joltageList = append(joltageList, jInt)
 		}
-	}
-	return edges
-}
 
-// More succinct that previous version 'brute force'
-// This is all we need to solve part 1
-func maxRectangleAreaAllPairs(verts []Point) int64 {
-	n := len(verts)
-	var best int64
+		// sort out buttons
+		buttons := elems[1 : n-1]
 
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			a := verts[i]
-			b := verts[j]
-			w := abs(a.X-b.X) + 1
-			h := abs(a.Y-b.Y) + 1
-			area := int64(w) * int64(h)
-			if area > best {
-				best = area
+		var buttonList [][]int
+		for _, j := range buttons {
+			var button []int
+
+			j = s.Replace(j, "(", "", 1)
+			j = s.Replace(j, ")", "", 1)
+			buttonElem := s.Split(j, ",")
+
+			for _, q := range buttonElem {
+				qInt, _ := strconv.Atoi(q)
+				button = append(button, qInt)
 			}
+			buttonList = append(buttonList, button)
 		}
+
+		data = append(data, Data{lights: lights, buttons: buttonList, joltages: joltageList})
 	}
-	return best
-}
-
-// Same as above, but if rectangle 'cuts' our polygon we discard it
-func maxRectangleAreaNoCut(verts []Point, edges []Edge) int64 {
-	n := len(verts)
-	var best int64
-
-	for i := 0; i < n; i++ {
-		for j := i + 1; j < n; j++ {
-			p := verts[i]
-			q := verts[j]
-
-			left := min(p.X, q.X)
-			right := max(p.X, q.X)
-			bottom := min(p.Y, q.Y)
-			top := max(p.Y, q.Y)
-
-			w := right - left + 1
-			h := top - bottom + 1
-			area := int64(w) * int64(h)
-			if area <= best {
-				continue
-			}
-
-			if rectangleCutByPolygon(left, right, bottom, top, edges) {
-				continue
-			}
-
-			best = area
-		}
-	}
-	return best
-}
-
-// rectangleCutByPolygon returns true if ANY polygon edge passes through the
-// interior of the rectangle [left,right] x [bottom,top]. Touching edges are OK.
-func rectangleCutByPolygon(left, right, bottom, top int, edges []Edge) bool {
-	for _, e := range edges {
-		if rectEdgeIntersect(left, right, bottom, top, e) {
-			return true
-		}
-	}
-	return false
-}
-
-// rectEdgeIntersect checks whether the edges of a rectangle intersect our polygon
-func rectEdgeIntersect(left, right, bottom, top int, e Edge) bool {
-	lx1, lx2 := e.A.X, e.B.X
-	if lx1 > lx2 {
-		lx1, lx2 = lx2, lx1
-	}
-	ly1, ly2 := e.A.Y, e.B.Y
-	if ly1 > ly2 {
-		ly1, ly2 = ly2, ly1
-	}
-
-	// If the edge is completely to left/right/below/above (including just touching), it's "away".
-	away :=
-		max(lx1, lx2) <= left || // entirely on or left of left side
-			min(lx1, lx2) >= right || // entirely on or right of right side
-			max(ly1, ly2) <= bottom || // entirely on or below bottom
-			min(ly1, ly2) >= top // entirely on or above top
-
-	// If NOT away, then this edge passes through the rectangle interior.
-	return !away
+	return data, scanner.Err()
 }
 
 // stupid wee helper functions that Go doesn't have
